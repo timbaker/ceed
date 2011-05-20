971ecfa70e69f6f26f54f5b5cc95dfc2a395af02
Martin Preisler <Martin Preisler@localhost>
2011-05-20 14:44:57 +0200

    MOD: runwrapper now forks a new bash shell so that debugging and all that is easily doable

--- runwrapper.sh
+++ runwrapper.sh
@@ -8,7 +8,10 @@
 # relative to where you run the script from or absolute (probably a more robust solution)
 CEGUI_BUILD_PATH="../cegui_mk2"
 
-export LD_LIBRARY_PATH="$CEGUI_BUILD_PATH/build/cegui/src/:$CEGUI_BUILD_PATH/build/cegui/src/RendererModules/OpenGL:$LD_LIBRARY_PATH"
-export PYTHONPATH="$CEGUI_BUILD_PATH/build/cegui/src/ScriptingModules/PythonScriptModule/bindings/:$PYTHONPATH"
+# Not needed with the new RPATH stuff in 0.8
+#export LD_LIBRARY_PATH="$CEGUI_BUILD_PATH/build/cegui/src/:$CEGUI_BUILD_PATH/build/cegui/src/RendererModules/OpenGL:$LD_LIBRARY_PATH"
 
-python $@
+export PYTHONPATH="$CEGUI_BUILD_PATH/build/lib:$PYTHONPATH"
+
+# fork a new shell with this PYTHONPATH to avoid polluting the environment
+bash
