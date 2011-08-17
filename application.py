8aa1e8b3d1323cf51143ee1ee0db40afb2e010aa
Martin Preisler <preisler.m@gmail.com>
2011-08-18 01:42:59 +0200

    FIX: http://cegui.org.uk/mantis/view.php?id=558

---
+++ application.py
@@ -0,0 +1,69 @@
+################################################################################
+#   CEED - A unified CEGUI editor
+#   Copyright (C) 2011 Martin Preisler <preisler.m@gmail.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+from PySide.QtCore import Qt
+from PySide.QtGui import QApplication, QSplashScreen, QPixmap
+
+import version
+
+class SplashScreen(QSplashScreen):
+    """A fancy splashscreen that fades out when user moves mouse over it or clicks it.
+    """
+    
+    def __init__(self):
+        super(SplashScreen, self).__init__(QPixmap("images/splashscreen.png"))
+
+        self.showMessage("(imageset editing implemented, limited layout editing possible!) | Version: %s" % (version.getAsString()), Qt.AlignTop | Qt.AlignRight, Qt.GlobalColor.white)
+
+class Application(QApplication):
+    """The central application class
+    """
+    
+    def __init__(self, argv):
+        super(Application, self).__init__(argv)
+
+        self.splash = SplashScreen()
+        self.splash.show()
+        
+        self.processEvents()
+        
+        # first recompile all UI files to ensure they are up to date
+        import compileuifiles
+        
+        compileuifiles.compileUIFiles("./ui")
+        compileuifiles.compileUIFiles("./ui/editors")
+        compileuifiles.compileUIFiles("./ui/editors/imageset")
+        compileuifiles.compileUIFiles("./ui/editors/layout")
+        compileuifiles.compileUIFiles("./ui/widgets")
+
+        self.setOrganizationName("CEGUI")
+        self.setOrganizationDomain("cegui.org.uk")
+        self.setApplicationName("CEED - CEGUI editor")
+        self.setApplicationVersion(version.getAsString())
+        
+        # import mainwindow after UI files have been recompiled
+        import mainwindow
+        
+        self.mainWindow = mainwindow.MainWindow(self)
+        self.mainWindow.showMaximized()
+        self.splash.finish(self.mainWindow)
+        
+        # import error after UI files have been recompiled
+        import error
+        self.errorHandler = error.ErrorHandler(self.mainWindow)
+        self.errorHandler.installExceptionHook()
---
+++ editors/text/__init__.py
@@ -0,0 +1,84 @@
+################################################################################
+#   CEED - A unified CEGUI editor
+#   Copyright (C) 2011 Martin Preisler <preisler.m@gmail.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+from PySide.QtGui import *
+import editors
+
+##
+# Multi purpose text editor
+#
+# TODO: This could get replaced by QScintilla once PySide guys get it to work.
+#       Scintilla would probably be overkill though, I can't imagine anyone
+#       doing any serious text editing in this application
+class TextTabbedEditor(editors.TabbedEditor):
+    def __init__(self, filePath):
+        
+        super(TextTabbedEditor, self).__init__(None, filePath)
+        
+        self.tabWidget = QTextEdit()
+    
+    def initialise(self, mainWindow):
+        super(TextTabbedEditor, self).initialise(mainWindow)
+            
+        file = open(self.filePath, "r")
+        self.textDocument = QTextDocument()
+        self.textDocument.setPlainText(file.read())
+        file.close()
+        
+        self.tabWidget.setDocument(self.textDocument)
+        self.textDocument.setModified(False)
+
+        self.textDocument.setUndoRedoEnabled(True)
+        self.textDocument.undoAvailable.connect(self.slot_undoAvailable)
+        self.textDocument.redoAvailable.connect(self.slot_redoAvailable)
+    
+    def finalise(self):
+        super(TextTabbedEditor, self).finalise()
+        
+        self.tabWidget = None
+        self.textDocument = None
+        
+    def hasChanges(self):
+        return self.textDocument.isModified()
+    
+    def undo(self):
+        # TODO: For some weird reason this doesn't do anything, could be a PySide bug
+        self.textDocument.undo()
+        
+    def redo(self):
+        # TODO: For some weird reason this doesn't do anything, could be a PySide bug
+        self.textDocument.redo()
+        
+    def slot_undoAvailable(self, available):
+        self.mainWindow.undoAction.setEnabled(available)
+        
+    def slot_redoAvailable(self, available):
+        self.mainWindow.redoAction.setEnabled(available)
+
+class TextTabbedEditorFactory(editors.TabbedEditorFactory):
+    def canEditFile(self, filePath):
+        extensions = ["py", "lua", "txt", "xml"]
+        
+        for extension in extensions:
+            if filePath.endswith("." + extension):
+                return True
+            
+        return False
+
+    def create(self, filePath):
+        return TextTabbedEditor(filePath)
