1dad6731cb5fc9a2adc9cd652526ff2f0b6a9ec9
Martin Preisler <Martin Preisler@localhost>
2011-10-16 01:49:16 +0200

    Add missing license clause to recentlyused.py

--- ceed/recentlyused.py
+++ ceed/recentlyused.py
@@ -1,11 +1,30 @@
+################################################################################
+#   CEED - A unified CEGUI editor
+#   Copyright (C) 2011 Martin Preisler <preisler.m@gmail.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+# Stefan Stammberger is the original author of this file
+
 from PySide.QtCore import *
 from PySide.QtGui import *
 
 class RecentlyUsed(object):
+    """This class can be used to store pointers to Items like files and images for later reuse within the application.
     """
-    This class can be used to store pointers to Items like files and images for later reuse within the application.
     
-    """
     def __init__(self, qsettings, sectionIdentifier):
         self.sectionIdentifier = "recentlyUsedIdentifier/" + sectionIdentifier
         self.qsettings = qsettings
