a5c6a1b32f2e6b4f1ddd511fd20697fa5ee3ce80
Pavlos Touboulidis <pavlos256@gmail.com>
2012-01-25 06:22:55 +0200

    Add GPLv3 blocks to the propertytree package.

--- ceed/propertytree/utility.py
+++ ceed/propertytree/utility.py
@@ -1,3 +1,21 @@
+################################################################################
+#   propertytree - A Qt property editor
+#   Copyright © 2012 Pavlos Touboulidis <pav256@gmail.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
 """Misc utilities.
 
 getDictionaryTreePath -- Retrieve a value from a dictionary tree.
