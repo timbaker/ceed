33aa972965b7066f44e7d1564dd061df597fbf73
Martin Preisler <martin@preisler.me>
2012-06-03 16:08:14 +0200

    Make the runwrapper.sh helper more robust (you don't have to do just ./ceed-gui anymore)

--- bin/runwrapper.sh
+++ bin/runwrapper.sh
@@ -5,10 +5,13 @@
 
 # Obviously doesn't work on Windows ;-) But works OK on Linux and MacOSX
 
+# parent dir of this script
+PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # relative to where you run the script from or absolute (probably a more robust solution)
-CEGUI_BUILD_PATH="../../cegui_mk2"
+CEGUI_BUILD_PATH="$PARENT_DIR/../../cegui_mk2"
 # directory where the "ceed" package is located
-CEED_PACKAGE_PATH="../"
+CEED_PACKAGE_PATH="$PARENT_DIR/../"
 
 # Not needed with the new RPATH stuff in 1.0
 #export LD_LIBRARY_PATH="$CEGUI_BUILD_PATH/build/cegui/src/:$CEGUI_BUILD_PATH/build/cegui/src/RendererModules/OpenGL:$LD_LIBRARY_PATH"
