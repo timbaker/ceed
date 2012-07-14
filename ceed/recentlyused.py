3dd4dd0e9a6305d096e81d1fc5fa256520451b8d
Martin Preisler <martin@preisler.me>
2012-07-15 01:26:43 +0200

    Avoid bloating recently used menus infinitely, minor pylint fixes

--- ceed/recentlyused.py
+++ ceed/recentlyused.py
@@ -18,24 +18,32 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##############################################################################
 
+"""Implements reusable functionality for "recently used" lists/menus.
+
+This is used mainly for "recent files" and "recent projects" in CEED.
+We may use it for even more in the future
+"""
+
 # Stefan Stammberger is the original author of this file
 
 from PySide import QtGui
 
 class RecentlyUsed(object):
-    """This class can be used to store pointers to Items like files and images for later reuse within the application.
+    """This class can be used to store pointers to Items like files and images
+    for later reuse within the application.
     """
 
     def __init__(self, qsettings, sectionIdentifier):
         self.sectionIdentifier = "recentlyUsedIdentifier/" + sectionIdentifier
         self.qsettings = qsettings
         # how many recent items should the editor remember
-        self.maxRecentlItems = 10
+        self.maxRecentItems = 10
         # to how many characters should the recent file names be trimmed to
         self.recentlItemsNameTrimLength = 60
 
     def addRecentlyUsed(self, itemName):
-        """ Add an item to the list """
+        """Add an item to the list"""
+
         items = []
         if self.qsettings.contains(self.sectionIdentifier):
             val = unicode(self.qsettings.value(self.sectionIdentifier))
@@ -54,19 +62,22 @@ class RecentlyUsed(object):
                 isInList = True
                 break
 
-        #only insert the file if it is not already in list,
+        # only insert the file if it is not already in list
         if not isInList:
             items.insert(0, itemName)
 
-        self.qsettings.setValue(self.sectionIdentifier, self.stringListToString(items))
-
         # while because items could be in a bad state because of previously thrown exceptions
         # make sure we trim them correctly in all circumstances
-        while len(items) > self.maxRecentlItems:
-            items.remove(items[self.maxRecentlItems])
+        while len(items) > self.maxRecentItems:
+            items.remove(items[self.maxRecentItems])
+
+        self.qsettings.setValue(self.sectionIdentifier, self.stringListToString(items))
 
     def removeRecentlyUsed(self, itemname):
-        """Removes an item from the list. Safe to call even if the item is not in the list."""
+        """Removes an item from the list. Safe to call even if the item is
+        not in the list.
+        """
+
         items = []
         if self.qsettings.contains(self.sectionIdentifier):
             val = unicode(self.qsettings.value(self.sectionIdentifier))
@@ -88,7 +99,8 @@ class RecentlyUsed(object):
         self.qsettings.remove(self.sectionIdentifier)
 
     def getRecentlyUsed(self):
-        """ Returns all items as a string list """
+        """Returns all items as a string list"""
+
         items = []
         if self.qsettings.contains(self.sectionIdentifier):
             val = unicode(self.qsettings.value(self.sectionIdentifier))
@@ -97,7 +109,8 @@ class RecentlyUsed(object):
         return items
 
     def trimItemName(self, itemName):
-        """ trim the itemName to the max. length and return it """
+        """Trim the itemName to the max. length and return it"""
+
         if (len(itemName) > self.recentlItemsNameTrimLength):
             # + 3 because of the ...
             trimedItemName = "...%s" % (itemName[-self.recentlItemsNameTrimLength + 3:])
@@ -105,12 +118,13 @@ class RecentlyUsed(object):
         else:
             return itemName
 
-    def stringListToString(self, list):
-        """ converts a list into a string for storage in QSettings """
+    def stringListToString(self, list_):
+        """Converts a list into a string for storage in QSettings"""
+
         temp = ""
 
         first = True
-        for s in list:
+        for s in list_:
             t = s.replace( ';', '\\;' )
             if first is True:
                 temp = t
@@ -121,9 +135,10 @@ class RecentlyUsed(object):
         return temp
 
     def stringToStringList(self, instr):
-        """ converts a string to a string list """
+        """Converts a string to a string list"""
+
         workStr = instr
-        list = []
+        list_ = []
 
         if not workStr.find('\\;') > -1:
             return instr.split(";")
@@ -131,42 +146,52 @@ class RecentlyUsed(object):
             tempList = []
             pos = workStr.find(';')
             while pos > -1:
-                #if we find a string that is escaped split the text and add it to the temporary list
-                #the path will be reconstructed when we don't find any escaped semicolons anymore (in else)
+                # if we find a string that is escaped split the text and add it
+                # to the temporary list
+                # the path will be reconstructed when we don't find any escaped
+                # semicolons anymore (in else)
+
                 if workStr[pos-1:pos+1] == "\\;":
                     tempList.append(workStr[:pos+1].replace("\\;", ";"))
                     workStr = workStr[pos+1:]
                     pos = workStr.find(';')
-                    if pos < 0: #end reached, finalize
-                        list.append(self.reconstructString(tempList) + workStr)
+                    if pos < 0: # end reached, finalize
+                        list_.append(self.reconstructString(tempList) + workStr)
                         workStr = workStr[pos+1:]
                         tempList = []
                 else:
-                    #found a unescaped ; so we reconstruct the string before it, it should be a complete item
-                    list.append(self.reconstructString(tempList) + workStr[:pos])
+                    # found a unescaped ; so we reconstruct the string before it,
+                    # it should be a complete item
+                    list_.append(self.reconstructString(tempList) + workStr[:pos])
                     workStr = workStr[pos+1:]
                     pos = workStr.find(';')
                     tempList = []
-            return list
 
+            return list_
 
-    def reconstructString(self, list):
-        """ reconstructs the string from a list and return it """
+    def reconstructString(self, list_):
+        """reconstructs the string from a list and return it"""
+
         reconst = ""
-        for s in list:
+        for s in list_:
             reconst = reconst + s
         return reconst
 
-
 class RecentlyUsedMenuEntry(RecentlyUsed):
+    """This class can be used to manage a Qt Menu entry to items.
     """
-    This class can be used to manage a Qt Menu entry to items.
-    """
+
     def __init__(self, qsettings, sectionIdentifier):
         super(RecentlyUsedMenuEntry, self).__init__(qsettings, sectionIdentifier)
 
+        self.menu = None
+        self.slot = None
+        self.clearAction = None
+
     def setParentMenu(self, menu, slot, clearAction):
-        """ sets the parent menu and the slot that is called when clicked on an item """
+        """Sets the parent menu and the slot that is called when clicked on an item
+        """
+
         self.menu = menu
         self.slot = slot
         self.clearAction = clearAction
@@ -174,12 +199,16 @@ class RecentlyUsedMenuEntry(RecentlyUsed):
         self.updateMenu()
 
     def addRecentlyUsed(self, itemName):
-        """ adds an item to the list """
+        """Adds an item to the list"""
+
         super(RecentlyUsedMenuEntry, self).addRecentlyUsed(itemName)
         self.updateMenu()
 
     def removeRecentlyUsed(self, itemName):
-        """Removes an item from the list. Safe to call even if the item is not in the list."""
+        """Removes an item from the list. Safe to call even if the item is not
+        in the list.
+        """
+
         super(RecentlyUsedMenuEntry, self).removeRecentlyUsed(itemName)
         self.updateMenu()
 
@@ -197,6 +226,7 @@ class RecentlyUsedMenuEntry(RecentlyUsed):
             text = self.trimItemName(f)
             if i <= 10:
                 text = "&" + str(i % 10) + ". " + text
+
             actionRP.setText(text)
             actionRP.setData(f)
             actionRP.setVisible(True)
