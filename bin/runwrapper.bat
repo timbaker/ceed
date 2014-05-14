23a2c3f60b2724395a5e255980afd8b9fb007c7e
Lukas Meindl <bitbucket@lukasmeindl.at>
2014-05-14 13:07:54 +0200

    MOD: Added ceed-gui starter to runwrapper

--- bin/runwrapper.bat
+++ bin/runwrapper.bat
@@ -1,4 +1,5 @@
 @echo off
+
 rem parent of this file
 set DIR_PARENT_OF_FILE=%~dp0
 
@@ -14,3 +15,5 @@ set PATH=%CEGUI_BUILD_PATH%/build/bin;%PATH%
 set PYTHONPATH=%CEGUI_BUILD_PATH%/build/bin;%CEED_PACKAGE_PATH%;%PYTHONPATH%
 
 start cmd.exe
+
+python ceed-gui
