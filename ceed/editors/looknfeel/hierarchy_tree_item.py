f3c07372136d7978bef2fac04e0a45d552e31111
Lukas Meindl <bitbucket@lukasmeindl.at>
2014-06-26 14:34:59 +0200

    MOD: Adding functions for the hierarchy creation

--- ceed/editors/looknfeel/hierarchy_tree_item.py
+++ ceed/editors/looknfeel/hierarchy_tree_item.py
@@ -30,16 +30,21 @@ import PyCEGUI
 
 class LookNFeelHierarchyItem(QtGui.QStandardItem):
 
-    def __init__(self, lookNFeelNode):
+    def __init__(self, widgetLookElement):
+        """
+        Creates a hierarchy item based on an element of the WidgetLookFeel object. The element can be
+        WidgetLookFeel itself or any child node (such as StateImagery or NamedArea, etc.) that is contained
+        in the WidgetLookFeel or any of its children. The children of this element will created as well. The
+        resulting hierarchy will be equal, or at least very similar, to the node hierarchy seen in the XML file.
+        :param widgetLookElement:
+        :return:
+        """
 
-        if lookNFeelNode is None:
-            super(LookNFeelHierarchyItem, self).__init__("None")
-            self.setToolTip("type: None")
-        if isinstance(lookNFeelNode, PyCEGUI.WidgetLookFeel):
-            super(LookNFeelHierarchyItem, self).__init__(lookNFeelNode.getName())
-            self.setToolTip("type: WidgetLookFeel")
+        self.widgetLookElement = widgetLookElement
+        name, toolTip = self.getNameAndToolTip(widgetLookElement)
 
-        # self.setData(manipulator.widget.getNamePath(), QtCore.Qt.UserRole)
+        super(LookNFeelHierarchyItem, self).__init__(name)
+        self.setToolTip(toolTip)
 
         self.setFlags(QtCore.Qt.ItemIsEnabled |
                       QtCore.Qt.ItemIsSelectable |
@@ -49,6 +54,241 @@ class LookNFeelHierarchyItem(QtGui.QStandardItem):
                       QtCore.Qt.ItemIsUserCheckable)
 
         self.setData(QtCore.Qt.Unchecked, QtCore.Qt.CheckStateRole)
+
+        self.createChildren()
+
+    @staticmethod
+    def getNameAndToolTip(widgetLookElement):
+        """
+        Creates a name and tooltip for any element that can be part of a WidgetLookFeel and returns it
+        :param widgetLookElement:
+        :return: str, str
+        """
+        name = u"None"
+        toolTip = u"type: None"
+
+        # The WidgetLookFeel object:
+        if isinstance(widgetLookElement, PyCEGUI.WidgetLookFeel):
+            mappedNameSplitResult = widgetLookElement.getName().split('/', 1)
+            name = mappedNameSplitResult[1]
+            toolTip = u"type: WidgetLookFeel"
+        # Objects that can be owned by a WidgetLookFeel:
+        elif isinstance(widgetLookElement, PyCEGUI.NamedArea):
+            name = u"NamedArea: " + widgetLookElement.getName() + u"\""
+            toolTip = u"type: NamedArea"
+        elif isinstance(widgetLookElement, PyCEGUI.ImagerySection):
+            name = u"ImagerySection: \"" + widgetLookElement.getName() + u"\""
+            toolTip = u"type: ImagerySection"
+        elif isinstance(widgetLookElement, PyCEGUI.StateImagery):
+            wlN = widgetLookElement.getName()
+            name = u"StateImagery: \"" + wlN + u"\""
+            toolTip = u"type: StateImagery"
+        elif isinstance(widgetLookElement, PyCEGUI.WidgetComponent):
+            name = u"Child : \"" + widgetLookElement.getWidgetName() + u"\""
+            toolTip = u"type: WidgetComponent"
+        # Objects that can be owned by a ImagerySection:
+        elif isinstance(widgetLookElement, PyCEGUI.ImageryComponent):
+            name = u"ImageryComponent"
+            toolTip = u"type: ImageryComponent"
+        elif isinstance(widgetLookElement, PyCEGUI.TextComponent):
+            name = u"TextComponent"
+            toolTip = u"type: TextComponent"
+        elif isinstance(widgetLookElement, PyCEGUI.FrameComponent):
+            name = u"FrameComponent"
+            toolTip = u"type: FrameComponent"
+        # Objects that can be owned by a StateImagery:
+        elif isinstance(widgetLookElement, PyCEGUI.LayerSpecification):
+            name = u"Layer"
+            toolTip = u"type: LayerSpecification"
+        # Objects that can be owned by a LayerSpecification:
+        elif isinstance(widgetLookElement, PyCEGUI.SectionSpecification):
+            name = u"Section"
+            toolTip = u"type: SectionSpecification"
+        # The ComponentArea element
+        elif isinstance(widgetLookElement, PyCEGUI.ComponentArea):
+            name = u"Area"
+            toolTip = u"type: ComponentArea"
+        # The ColourRect element
+        elif isinstance(widgetLookElement, PyCEGUI.ColourRect):
+            name = u"Colours"
+            toolTip = u"type: ColourRect"
+
+        return name, toolTip
+
+    def createChildren(self):
+        """
+        Creates the children items for for the this item's WidgetLookFeel element
+        :param:
+        :return:
+        """
+
+        if isinstance(self.widgetLookElement, PyCEGUI.WidgetLookFeel):
+            self.createWidgetLookFeelChildren()
+        elif isinstance(self.widgetLookElement, PyCEGUI.NamedArea):
+            self.createNamedAreaChildren()
+        elif isinstance(self.widgetLookElement, PyCEGUI.ImagerySection):
+            self.createImagerySectionChildren()
+        elif isinstance(self.widgetLookElement, PyCEGUI.StateImagery):
+            self.createStateImageryChildren()
+        elif isinstance(self.widgetLookElement, PyCEGUI.WidgetComponent):
+            self.createWidgetComponentChildren()
+        elif isinstance(self.widgetLookElement, PyCEGUI.ImageryComponent):
+            self.createImageryComponentChildren()
+        elif isinstance(self.widgetLookElement, PyCEGUI.TextComponent):
+            self.createTextComponentChildren()
+        elif isinstance(self.widgetLookElement, PyCEGUI.FrameComponent):
+            self.createFrameComponentChildren()
+        elif isinstance(self.widgetLookElement, PyCEGUI.LayerSpecification):
+            self.createLayerSpecificationChildren()
+        elif isinstance(self.widgetLookElement, PyCEGUI.SectionSpecification):
+            self.createSectionSpecificationChildren()
+
+    def createAndAddItem(self, widgetLookElement):
+        """
+        Creates an item based on the supplied object and adds it to this object
+        :param widgetLookElement:
+        :return:
+        """
+        newItem = LookNFeelHierarchyItem(widgetLookElement)
+        self.appendRow(newItem)
+
+    def createWidgetLookFeelChildren(self):
+        """
+        Iterates over all contained elements of the WidgetLookFeel. Creates the contained elements as
+        new items and appends them to this item
+        :return:
+        """
+        namedAreaIter = self.widgetLookElement.getNamedAreaIterator()
+        while not namedAreaIter.isAtEnd():
+            currentNamedArea = namedAreaIter.getCurrentValue()
+            self.createAndAddItem(currentNamedArea)
+            namedAreaIter.next()
+
+        imageryIter = self.widgetLookElement.getImageryIterator()
+        while not imageryIter.isAtEnd():
+            currentImagerySection = imageryIter.getCurrentValue()
+            self.createAndAddItem(currentImagerySection)
+            imageryIter.next()
+
+        stateIter = self.widgetLookElement.getStateIterator()
+        while not stateIter.isAtEnd():
+            currentStateImagery = stateIter.getCurrentValue()
+            self.createAndAddItem(currentStateImagery)
+            stateIter.next()
+
+        childWidgetIter = self.widgetLookElement.getWidgetComponentIterator()
+        while not childWidgetIter.isAtEnd():
+            currentChildWidget = childWidgetIter.getCurrentValue()
+            self.createAndAddItem(currentChildWidget)
+            childWidgetIter.next()
+
+    def createNamedAreaChildren(self):
+        """
+        Creates and appends an item for the Area of the NamedArea
+        :return:
+        """
+        area = self.widgetLookElement.getArea()
+        self.createAndAddItem(area)
+
+    def createImagerySectionChildren(self):
+        """
+        Creates and appends children items based on an ImagerySection.
+        :return:
+        """
+        frameCompIter = self.widgetLookElement.getFrameComponentIterator()
+        while not frameCompIter.isAtEnd():
+            currentFrameComp = frameCompIter.getCurrentValue()
+            self.createAndAddItem(currentFrameComp)
+            frameCompIter.next()
+
+        textCompIter = self.widgetLookElement.getTextComponentIterator()
+        while not textCompIter.isAtEnd():
+            currentTextComp = textCompIter.getCurrentValue()
+            self.createAndAddItem(currentTextComp)
+            textCompIter.next()
+
+        imageryCompIter = self.widgetLookElement.getImageryComponentIterator()
+        while not imageryCompIter.isAtEnd():
+            imageryComp = imageryCompIter.getCurrentValue()
+            self.createAndAddItem(imageryComp)
+            imageryCompIter.next()
+
+        coloursRect = self.widgetLookElement.getMasterColours()
+        self.createAndAddItem(coloursRect)
+
+    def createStateImageryChildren(self):
+        """
+        Creates and appends children items based on an ImagerySection.
+        :return:
+        """
+        layerIter = self.widgetLookElement.getLayerIterator()
+        while not layerIter.isAtEnd():
+            layer = layerIter.getCurrentValue()
+            self.createAndAddItem(layer)
+            layerIter.next()
+
+    def createWidgetComponentChildren(self):
+        """
+        Creates and appends children items based on a WidgetComponent (child widget).
+        :return:
+        """
+        area = self.widgetLookElement.getComponentArea()
+        self.createAndAddItem(area)
+
+    def createImageryComponentChildren(self):
+        """
+        Creates and appends children items based on an ImageryComponent.
+        :return:
+        """
+        area = self.widgetLookElement.getComponentArea()
+        self.createAndAddItem(area)
+
+
+    def createTextComponentChildren(self):
+        """
+        Creates and appends children items based on a TextComponent.
+        :return:
+        """
+        area = self.widgetLookElement.getComponentArea()
+        self.createAndAddItem(area)
+
+    def createFrameComponentChildren(self):
+        """
+        Creates and appends children items based on a FrameComponent.
+        :return:
+        """
+        area = self.widgetLookElement.getComponentArea()
+        self.createAndAddItem(area)
+
+        testWindow = PyCEGUI.WindowManager.getSingleton().createWindow("DefaultWindow", "")
+
+        image = self.widgetLookElement.getImage(PyCEGUI.FrameImageComponent.FIC_TOP_LEFT_CORNER, testWindow)
+        self.createAndAddItem(image)
+        image = self.widgetLookElement.getImage(PyCEGUI.FrameImageComponent.FIC_TOP_RIGHT_CORNER, testWindow)
+        self.createAndAddItem(image)
+        image = self.widgetLookElement.getImage(PyCEGUI.FrameImageComponent.FIC_BOTTOM_LEFT_CORNER, testWindow)
+        self.createAndAddItem(image)
+        image = self.widgetLookElement.getImage(PyCEGUI.FrameImageComponent.FIC_BOTTOM_RIGHT_CORNER, testWindow)
+        self.createAndAddItem(image)
+        image = self.widgetLookElement.getImage(PyCEGUI.FrameImageComponent.FIC_LEFT_EDGE, testWindow)
+        self.createAndAddItem(image)
+        image = self.widgetLookElement.getImage(PyCEGUI.FrameImageComponent.FIC_RIGHT_EDGE, testWindow)
+        self.createAndAddItem(image)
+        image = self.widgetLookElement.getImage(PyCEGUI.FrameImageComponent.FIC_TOP_EDGE, testWindow)
+        self.createAndAddItem(image)
+        image = self.widgetLookElement.getImage(PyCEGUI.FrameImageComponent.FIC_BOTTOM_EDGE, testWindow)
+        self.createAndAddItem(image)
+        image = self.widgetLookElement.getImage(PyCEGUI.FrameImageComponent.FIC_BACKGROUND, testWindow)
+        self.createAndAddItem(image)
+
+        colourRect = self.widgetLookElement.getColours()
+        self.createAndAddItem(colourRect)
+
+    def createLayerSpecificationChildren(self):
+        return
+
+    def createSectionSpecificationChildren(self):
+        return
 
     def clone(self):
         ret = LookNFeelHierarchyItem(self.manipulator)
